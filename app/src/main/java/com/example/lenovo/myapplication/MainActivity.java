bab094ca1d120e4f957b86f188041c03ff59a8ce
ysk <1003889304@qq.com>
2016-09-21 15:35:01 +0800

    1，bdmessage

--- app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText BDcard;
     private EditText BDtext;
     private Button send;
-    private TextView BDs;  //勾勾撒旦
+    private TextView BDs;  //适当放松放松
     private static int msgId = 1;
     private LocationManager lm;
 
